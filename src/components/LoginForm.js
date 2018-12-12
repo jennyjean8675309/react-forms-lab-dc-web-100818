91dd0279260234da6c197d8fdc5da260ceff80bf
jennyjean <jennyjean8675309@yahoo.com>
2018-12-12 16:29:33 -0500

    Used form event listeners to update state and callback functions to pass a components state to a parental function

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,42 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
 
+  handleInputChange = (event) =>{
+    this.setState({
+      [event.target.name]: "" + event.target.value
+    })
+  }
+
+  //LoginForm receives a prop(onSubmit) from App, which is equal to a
+  //function object (login).
+  //When the onSubmit event is fired, LoginForm can invoke that function
+  //through props (this.props.onSubmit) with it's current state passed in
+  //as the argument to the login function.
+  //The login function is destructured, meaning that it expects an object
+  //{ username, password } with two key/value pairs
   render() {
     return (
-      <form>
+      <form onSubmit={ event =>{
+           event.preventDefault()
+           this.state.username !== "" && this.state.password !== "" ?
+           (this.props.onSubmit({username: this.state.username, password: this.state.password})) : null
+         }}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleMessageChange = (event) =>{
+    this.setState({
+      message: "" + `${event.target.value}`
+    })
+    this.remainingCharacters(event)
+  }
+
+  remainingCharacters = (event) =>{
+    return (this.props.maxChars - this.state.message.length)
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.message}
+        onChange={event => this.handleMessageChange(event)}/>
+      <p>You have {this.remainingCharacters()} characters remaining.</p>
       </div>
     );
   }
